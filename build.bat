52758b40057ac2e353a751cd5d86e092fb65577a
Srini Vasudevan <srini@gofundraise.com>
2021-01-08 17:24:11 +1100

    update build

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+docker build -t srini85/aws-cli-ci:node8 -f Dockerfile.Node12 .
 docker build -t srini85/aws-cli-ci:node8 -f Dockerfile.Node8 .
 docker build -t srini85/aws-cli-ci:latest -f Dockerfile .
+docker push srini85/aws-cli-ci:node12
 docker push srini85/aws-cli-ci:node8
 docker push srini85/aws-cli-ci:latest
