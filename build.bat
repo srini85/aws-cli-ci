27843967df64efba56addab2fdeb0de49ad85bd0
Srini Vasudevan <srini@gofundraise.com>
2022-06-02 11:32:09 +1000

    include node 16 image

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-docker build -t srini85/aws-cli-ci:node8 -f Dockerfile.Node12 .
+docker build -t srini85/aws-cli-ci:node12 -f Dockerfile.Node12 .
+docker build -t srini85/aws-cli-ci:node12 -f Dockerfile.Node16 .
 docker build -t srini85/aws-cli-ci:node8 -f Dockerfile.Node8 .
 docker build -t srini85/aws-cli-ci:latest -f Dockerfile .
+docker push srini85/aws-cli-ci:node16
 docker push srini85/aws-cli-ci:node12
 docker push srini85/aws-cli-ci:node8
 docker push srini85/aws-cli-ci:latest
